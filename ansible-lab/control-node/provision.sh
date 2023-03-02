c33b02e022f2b1ae329cee56ce6c64ac5603de20
adrian_s_lps <adriansoareslps@gmail.com>
2023-03-02 14:21:08 -0300

    Finished ansible deploy code

--- ansible-lab/control-node/provision.sh
+++ ansible-lab/control-node/provision.sh
@@ -3,8 +3,16 @@
 sudo yum -y install epel-release #repository for ansible in centos
 echo "Installing Ansible..."
 sudo yum -y install ansible
-cat <<EOT >> /etc/hosts
+cat <<EOM >> /etc/hosts
 192.168.56.2 control-node
 192.168.56.3 app01
 192.168.56.4 db01
-EOT
+EOM
+
+cat >> /etc/ansible/hosts <<- EOM
+[apps]
+app01
+
+[dbs]
+db01
+EOM  
