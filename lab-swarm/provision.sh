fc7c8ce5447ff510711c9835d8f11343867c2dfa
adrian_s_lps <adriansoareslps@gmail.com>
2023-03-09 10:00:03 -0300

    Test for Docker Swarm

---
+++ lab-swarm/provision.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo curl -fsSL "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/
+
+cat <<-EOM >> /etc/hosts 
+  192.168.56.2 manager
+  192.168.56.3 workerApp
+  192.168.56.4 workerDb
+EOM
