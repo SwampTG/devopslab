059e91583c353115bc087aa6a64409bcfe3f171b
adrian_s_lps <adriansoareslps@gmail.com>
2023-03-10 12:18:31 -0300

    Adds sonnar-scanner

--- lab-jenkins/provision.sh
+++ lab-jenkins/provision.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo yum install -q epel-release java-11-openjdk-devel yum-utils git -y
+SONAR_SCANNER_VERSION="4.8.0.2856"
+
+sudo yum install -q epel-release java-11-openjdk-devel yum-utils git unzip -y
 
 sudo curl -fsSL https://pkg.jenkins.io/redhat-stable/jenkins.repo -o /etc/yum.repos.d/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
@@ -19,4 +21,11 @@ systemctl daemon-reload
 systemctl restart docker
 
 usermod -aG docker jenkins
-sudo service start jenkins
+sudo service jenkins start
+sudo systemctl enable jenkins
+
+curl -fsSL "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip" -o scanner.zip && sudo unzip scanner.zip -d /opt/
+sudo mv "/opt/sonar-scanner-$SONAR_SCANNER_VERSION-linux" /opt/sonar-scanner
+echo 'export PATH=$PATH:/opt/sonar-scanner/bin' | sudo tee -a /etc/profile
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+sudo yum install -q nodejs -y
